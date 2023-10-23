a830f8f10d6bd222fa5108f67ec5b1bdbba91b4b
gagugo <82519190+gagugo@users.noreply.github.com>
2023-10-23 18:29:36 +0200

    Update pythom_2_ukol_3.py

--- pythom_2_ukol_3.py
+++ pythom_2_ukol_3.py
@@ -19,3 +19,16 @@ json_object = json.dumps(prospech, indent=4, ensure_ascii=False)
 # Writing to sample.json
 with open("sample.json", "w", encoding="utf-8") as outfile:
     outfile.write(json_object)
+
+##komentar M.Jurosz
+##ukol ti nevraci slovnik ale pole, coz je vlastne slozitejsi a pro nase ucely na dalsi zpracovani neni pole moc idealni
+##nize je nahrada kodu mezi radky 9 - 21 a je tam pozadovany vystup. Kdyz tak vyzkousej a dej vedet zda je to pochopitelne nebo to muzem probrat
+
+# hodnoceni = {}
+# for student in f:
+    # if f[student] >= 60:
+        # hodnoceni[student] = "Pass"
+    # else:
+        # hodnoceni[student] = "Fail"
+# with open('prospech.json', mode='w', encoding='utf-8') as out_file:
+    # json.dump(hodnoceni, out_file, indent=4, ensure_ascii=False)
